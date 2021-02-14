b98917740c29119956648b21d204b78d45e34298
Eric Grossman <eric.josh.grossman@gmail.com>
2021-02-14 17:00:27 -0600

    initial

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,4 @@
 import { Dispatch, AnyAction } from 'redux';
 declare type MiddlewareReturn = (next: Dispatch<AnyAction>) => (action: any) => any;
-export declare function expoReduxLogger({ getState }: any): MiddlewareReturn;
+export declare function expoLogger({ getState }: any): MiddlewareReturn;
 export {};
